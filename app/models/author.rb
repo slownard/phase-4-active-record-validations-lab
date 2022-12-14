63f0e02b6e5eda07b09c4e76b16cbdd6188ac07d
Bernard Legaspi <blegaspi26@gmail.com>
2022-12-14 11:45:33 -0500

    commit tree 7b5a2d277f80ae79f1476af888d054262c24ab2d

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,24 @@
 class Author < ApplicationRecord
+
+    #add validations
+
+
+    #all authors have a name
+
+    #no 2 authors have the same name 
+
+    #phone numbers have exactly 10 digits
+
+    #must have a title 
+     
+    #post content is at least 250 characters long
+
+    #summary max is 250 characters
+
+    #category is either fiction or nonfiction, requieres an 'inclusion' valdiator
+
+
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+
 end
